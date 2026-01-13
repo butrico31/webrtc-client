c88b23eecae73e8d492755c81dd0f805010020d1
butrico31 <butrico0@gmail.com>
2026-01-13 14:07:28 -0300

    feat: atualizar a URL da API AMI para o ambiente de produção

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const SIP_CONFIG = {
   // Mock estático para testes
   domain: "srv762442.hstgr.cloud",
   wssUrl: "wss://srv762442.hstgr.cloud:8089/ws",
-  amiApiBaseUrl: "http://localhost:3001",
+  amiApiBaseUrl: "https://srv762442.hstgr.cloud:3002",
   freeExtensionsPath: "/extensions/free",
   countryId: "55",
 
